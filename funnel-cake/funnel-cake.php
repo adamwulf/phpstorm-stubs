6a87dc1c6426b4f3e6c290e94772221e298c7b1d
Adam Wulf <adam.wulf@gmail.com>
2019-09-16 23:22:55 -0500

    Added return type

--- funnel-cake/funnel-cake.php
+++ funnel-cake/funnel-cake.php
@@ -1,11 +1,6 @@
 <?php
 
-
-function exception_error_handler(int $errno, string $errstr, string $errfile, int $errline ) : void { }
-
-
 function registerClassMethodsWithNewRelic($object) : void{ }
-
 
 function registerTransactionNameWithNewRelic(string $name) : void{ }
 
@@ -16,3 +11,21 @@ function str_has_suffix(string $whole, string $end) : bool { }
 function describe($anything) : string { }
 
 function generateDateGroupsBetween(MMDateTime $dt, MMDateTime $endDate) : array { }
+
+function assertReturnType(Closure $lambda, string $type) : void { }
+
+function assertParameterCount(Closure $lambda, int $count) : void { }
+
+function assertParameterType(Closure $lambda, int $index, string $type) : void { }
+
+function parseFragment(string $fragment) : ?object { }
+
+function fragmentForTypeAndId(string $type, int $id) : string { }
+
+function enumerateDirectory(string $directory, Closure $lamda) : void { }
+
+function enumerateDirectories(array $directories, Closure $lamda) : void { }
+
+function enumerateInDirectory(string $directory, string $extension, Closure $lambda) : void { }
+
+function includeInDirectory(string $directory, string $extension) : void { }
